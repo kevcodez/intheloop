eb7c01b9baa344a2d1d0e31b6071abf0f9f692a4
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-02-25 22:13:35 +0100

    Netlify form submit

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -3,31 +3,24 @@
     <form
       class="space-y-4 max-w-3xl"
       name="contact"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
+      @submit.prevent="submitForm"
     >
-      <p class="hidden">
-        <label
-          >Don’t fill this out if you’re human: <input name="bot-field"
-        /></label>
-      </p>
-      <input type="hidden" name="form-name" value="contact" />
       <div>
         If you see any invalid information, would like to see another topic or
         have suggestions on how to improve this page, let me know
       </div>
       <div>
         <label class="form-label">Email (optional)</label>
-        <input class="input" type="email" name="email" />
+        <input class="input" type="email" v-model="email" />
       </div>
       <div>
         <label class="form-label">Text</label>
         <textarea
+          v-model="message"
           class="input h-40"
           style="min-height: 150px"
-          name="message"
           required
-          :minlength="30"
+          :minlength="20"
         />
       </div>
       <div>
@@ -36,3 +29,29 @@
     </form>
   </div>
 </template>
+
+<script lang="ts" setup>
+import ky from "ky";
+import { ref } from "vue";
+
+const email = ref("");
+const message = ref("");
+
+const submitForm = () => {
+  const formData = new FormData();
+  formData.append("email", email.value);
+  formData.append("message", message.value);
+
+  ky.post("/", {
+    body: formData,
+  })
+    .then((response) => {
+      if (response.status === 200) {
+        alert("E-mail Sent!");
+      } else {
+        throw new Error(response.statusText);
+      }
+    })
+    .catch(() => alert("Failed to sent E-mail"));
+};
+</script>
