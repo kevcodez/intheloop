73b9df48628d73c93b30be56f02a14c52aea8ab0
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-02-27 18:32:12 +0100

    Switch font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge:  ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'class',
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  }
 }
